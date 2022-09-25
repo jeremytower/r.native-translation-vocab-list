3046ed0a9fdd0226812e999c3a79dd96f5478284
Jeremy Tower <jeremy_tower@yahoo.com>
2022-09-25 14:54:46 -0400

    Added my project

--- App.js
+++ App.js
@@ -1,20 +1,558 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, TextInput, View, Button, TouchableOpacity, Alert } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import IonIcon from 'react-native-vector-icons/Ionicons';
+
+
 
 export default function App() {
+  const [wordList, setWordList] = useState([]);
+  const [filterString, setFilterString] = useState('');
+  const [engText, setEngText] = useState('');
+  const [espText, setEspText] = useState('');
+  const [showAdd, setShowAdd] = useState(false);
+  const [showImport, setShowImport] = useState(false);
+  const [engMode, setEngMode] = useState(false);
+  const [showTrnsl, setShowTrnsl] = useState(true);
+  const [uploadString, setUploadString] = useState('');
+
+  const editIconSize = 20;
+
+  function sortList(list){
+    list.sort((a, b) => {
+      let aFld = engMode ? a.eng.toLowerCase() : a.esp.toLowerCase(),
+          bFld = engMode ? b.eng.toLowerCase() : b.esp.toLowerCase();
+  
+      if ( aFld < bFld) {
+          return -1;
+      }
+      if (aFld > bFld) {
+          return 1;
+      }
+      return 0;
+    });
+  }
+
+  const _loadItems = async () => {
+    console.log("hard load")
+    try{
+      const keydata = await AsyncStorage.getAllKeys();
+      const storedData = await AsyncStorage.multiGet(keydata);
+      const loadedWordList = [];
+      storedData.forEach(storedItem => {
+        const loadedWordItem = {
+          eng: storedItem[0],
+          esp: storedItem[1],
+          newEng: storedItem[0],
+          newEsp: storedItem[1],
+          editMode: false
+        };
+        loadedWordList.push(loadedWordItem);
+      });
+      sortList(loadedWordList);
+      setWordList(loadedWordList);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  const _storeItem = async (item) => {
+    try {
+      await AsyncStorage.setItem(item.eng, item.esp);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  const _deleteItem = async (item) => {
+    try {
+      await AsyncStorage.removeItem(item.eng);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  const _updateItem = async (item) => {
+    try {
+      await AsyncStorage.removeItem(item.eng);
+      await AsyncStorage.setItem(item.newEng, item.newEsp);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  const _saveImport = async (insertList) => {
+    try {
+      await AsyncStorage.multiSet(insertList, _loadItems);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  const _deleteAllData = async () => {
+    try {
+      await AsyncStorage.clear(_loadItems);
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
+  
+  useEffect(() => {
+    //_loadItems(); no longer need to load on start because setting the state of engMode on open provides first loaditems call    
+  }, []);
+
+  useEffect(() => {
+    _loadItems();   
+  }, [engMode]);
+
+  function validEntry(){
+    const engTextTrimmed= engText.trim(),
+          espTextTrimmed= espText.trim();
+    if(engTextTrimmed && espTextTrimmed){ 
+      const dup = wordList.some(value => value.eng === engTextTrimmed && value.esp === espTextTrimmed);
+      if(!dup){return true;}
+    }
+    return false;
+  }
+
+  function langButtonClick(){
+    setEngMode(!engMode);
+  }
+  
+  function onAddWordClick(){
+    addWord();
+  }
+  
+  function importItems(){
+    var cleanUploadString = uploadString.replace('\n', '').trim();
+    if(cleanUploadString.slice(-1) == ','){cleanUploadString = cleanUploadString.slice(0, -1);}
+    const itemsArr = cleanUploadString.split(',');
+    const insertList = [];
+    itemsArr.forEach(item => {
+      const splitItem = item.split('-').reverse();
+      const cleanedSplitItem = [];
+      cleanedSplitItem.push(splitItem[0].replace('\n', '').trim());
+      cleanedSplitItem.push(splitItem[1].replace('\n', '').trim());
+      insertList.push(cleanedSplitItem);
+    });
+    _saveImport(insertList);
+  }
+
+  function exportItems(){
+    var newUploadString = '';
+    wordList.forEach(item => {
+      newUploadString = newUploadString + item.esp + ' - ' + item.eng + ',\n';
+    });
+    newUploadString = newUploadString.slice(0, -2); //get rid of last ,\n
+    setUploadString(newUploadString);
+  }
+
+  function deleteAll(){
+    Alert.alert("Permanently delete all data?", "I reccommend generating an import string and saving in your notepad first",
+    [{
+      text: "Delete all",
+      onPress: () => {_deleteAllData();},
+    },
+    {
+      text: "Cancel",  
+    }]
+    );
+    
+  }
+
+  function addWord(){
+    if(validEntry()){
+      const trimmedEng = engText.trim(), trimmedEsp = espText.trim();
+      const newItem = {eng: trimmedEng, esp: trimmedEsp, newEng: trimmedEng, newEsp: trimmedEsp, editMode: false};
+      const newList = wordList.slice();
+      newList.push(newItem);
+      sortList(newList);
+      setWordList(newList);
+      setEspText('');
+      setEngText('');
+      _storeItem(newItem);
+    }
+  }
+  function tapListItem(item){
+    if(!item.editMode){
+      const newList = wordList.slice().filter(word => !(word.eng === item.eng && word.esp === item.esp));
+      item.editMode = true;
+      newList.push(item);
+      sortList(newList);
+      setWordList(newList);
+    }
+  }
+  function saveListItem(item){
+    if(item.editMode){
+      const newList = wordList.slice().filter(word => !(word.eng === item.eng && word.esp === item.esp));
+      const newItem = structuredClone(item);
+      newItem.editMode = false, newItem.eng = newItem.newEng, newItem.esp = newItem.newEsp;
+      newList.push(newItem);
+      sortList(newList);
+      setWordList(newList);
+      _updateItem(item);
+    }
+  }
+  function deleteListItem(item){
+    if(item.editMode){
+      const newList = wordList.slice().filter(word => !(word.eng === item.eng && word.esp === item.esp));
+      sortList(newList);
+      setWordList(newList);
+      _deleteItem(item);
+    }
+  }
+  function closeListItem(item){
+    if(item.editMode){
+      const newList = wordList.slice().filter(word => !(word.eng === item.eng && word.esp === item.esp));
+      item.editMode = false;
+      newList.push(item);
+      sortList(newList);
+      setWordList(newList);
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container}> 
+      <ButtonBar showAdd={showAdd} showImport={showImport} engMode={engMode} showTrnsl={showTrnsl} 
+        setShowAdd={setShowAdd} setShowImport={setShowImport} setEngMode={setEngMode} setShowTrnsl={setShowTrnsl} 
+        langButtonClick={langButtonClick} deleteAll={deleteAll} />
+      
+      <AddSection showAdd={showAdd} engText={engText} espText={espText} 
+        setEngText={setEngText} setEspText={setEspText} onAddWordClick={onAddWordClick} />
+      <ImportSection showImport={showImport} uploadString={uploadString} 
+        importItems={importItems} exportItems={exportItems} setUploadString={setUploadString}/>
+      <TextInput style={styles.filterBox} defaultValue={filterString} onChangeText={newText => setFilterString(newText)} placeholder='filter...' />
+      <View style={styles.listSection}>
+        <FlatList data={wordList.filter(word => word.eng.toLowerCase().includes(filterString.toLocaleLowerCase()) 
+                                        || word.esp.toLowerCase().includes(filterString.toLocaleLowerCase()))}
+          extraData={wordList}
+          renderItem={({item}) => 
+            <ListItem engMode={engMode} showTrnsl={showTrnsl} item={item} editIconSize={editIconSize}
+            tapListItem={tapListItem} saveListItem={saveListItem} closeListItem={closeListItem} deleteListItem={deleteListItem}  />
+          }
+        />
+      </View>
     </View>
   );
 }
 
+function ListItem(props) {
+  if(!props.item.editMode){
+    return(
+      <TouchableOpacity onPress={() => props.tapListItem(props.item)}>
+        <Text style={styles.item}>
+          { (props.engMode ? props.item.eng : props.item.esp)  + (props.showTrnsl ? " - " + (props.engMode? props.item.esp : props.item.eng) : '') }
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+  else{
+    return(
+        <View style={styles.itemEditMode}>
+          <View style={styles.itemEditTextBoxWrapper}>
+            <TextInput
+                style={styles.itemEditTextbox}
+                defaultValue = {props.engMode ? props.item.newEng : props.item.newEsp}
+                onChangeText={newText => (props.engMode ? props.item.newEng = newText : props.item.newEsp = newText)}
+              />
+          </View>
+          <View style={styles.itemEditTextBoxWrapper}>
+            <TextInput
+              style={styles.itemEditTextbox}
+              defaultValue = {props.engMode ? props.item.newEsp : props.item.newEng}
+              onChangeText={newText => (props.engMode ? props.item.newEsp = newText : props.item.newEng = newText)}
+            />
+          </View>
+          <View style={styles.editItemButtonBar}>
+            <TouchableOpacity style={[styles.editItemButton, styles.editItemSaveButton]} onPress={()=>props.saveListItem(props.item)}>
+              <Icon
+                name='save' 
+                size={props.editIconSize}
+                color='black'/>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.editItemButton, styles.editItemDeleteButton]} onPress={()=>props.deleteListItem(props.item)}>
+            <Icon
+                name='trash' 
+                size={props.editIconSize}
+                color='black'/>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.editItemButton, styles.editItemCloseButton]} onPress={()=>props.closeListItem(props.item)}>
+            <Icon
+                name='close' 
+                size={props.editIconSize}
+                color='black'/>
+            </TouchableOpacity>
+          </View>
+      </View> 
+    );
+  }
+}
+
+function ButtonBar(props) {
+  return(
+    <View style={styles.buttonBarWrapper} >
+      <TouchableOpacity style={[styles.topButton, props.showAdd ? styles.showAddButtonOpen: styles.showAddButtonClosed]} 
+        onPress={() => props.setShowAdd(!props.showAdd)}>
+        <Icon
+          name={props.showAdd ? 'close' : 'plus'}
+          size={15}
+          color={'white'}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.topButton, styles.langButton]} 
+        onPress={() => props.langButtonClick()} >
+        <AntDesign
+          name={props.engMode ? 'shrink' : 'arrowsalt'}
+          size={15}
+          color={'white'}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{...styles.topButton, backgroundColor: props.showTrnsl ? '#E34B4D' : '#32BF80' }} 
+        onPress={() => props.setShowTrnsl(!props.showTrnsl)} >
+        <IonIcon
+          name={props.showTrnsl ? 'eye-off' : 'eye'}
+          size={15}
+          color={'white'}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.topButton, props.showImport ? styles.showImportButtonOpen: styles.showImportButtonClosed]} 
+        onPress={() => props.setShowImport(!props.showImport)}>
+        <Icon
+          name={props.showImport ? 'close' : 'file-text'}
+          size={15}
+          color={'white'}/>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={[styles.topButton, styles.killButton]} 
+        onPress={() => props.deleteAll()} >
+        <MaterialIcons
+          name={'local-fire-department'}
+          size={20}
+          color={'#D42222'}/>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function AddSection(props) {
+  if(props.showAdd){
+    return(
+      <> 
+        <View style={styles.inputGroup}>
+          <Text style={styles.headerText}>English:</Text>
+          <TextInput
+            style={styles.textBox}
+            onChangeText={newText => props.setEngText(newText)}
+            value = {props.engText}
+          />
+        </View>
+  
+        <View style={styles.inputGroup}>
+          <Text style={styles.headerText}>Spanish:</Text>
+          <TextInput
+            style={styles.textBox}
+            onChangeText={newText => props.setEspText(newText)}
+            value = {props.espText}
+          />
+        </View>
+        
+        <View style={styles.addButton}>
+          <Button onPress={() => props.onAddWordClick()} 
+            title="Add Word"
+            color="#50A3DE" />
+        </View>
+      </>
+    );
+  }
+  else{return(<></>);}
+}
+function ImportSection(props) {
+  if(props.showImport){
+    return(
+      <>
+      <View style={styles.importTextboxWrapper}>
+        <TextInput
+                style={[styles.textBox, styles.importTextBox]}
+                defaultValue = {props.uploadString}
+                onChangeText={newText => props.setUploadString(newText)}
+                multiline={true}
+                numberOfLines={10}
+              />
+        </View>
+        <Text style={styles.importExampleText}>Format: Lang1Word1: Lang2Word1, Lang1Word2: Lang2Word2 etc...</Text>
+        <View style={styles.importButtons}>
+          <View style={styles.importButton}>
+            <Button onPress={() => props.exportItems()} 
+              title="Generate string"
+              color="#50A3DE" />
+          
+          </View>
+          <View style={styles.importButton}>
+            <Button onPress={() => props.importItems()} 
+              title="Import string"
+              color="#50A3DE" />
+          </View>
+        </View>
+        </>
+    );
+  }
+  else{return(<></>);}
+}
+const editHeight = 40;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    marginTop: 50,
+    marginLeft: 30
+  },
+  buttonBarWrapper: {
+    justifyContent: 'flex-start',
+    height: 50,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: '100%',
+    marginTop: 15,
+    marginBottom: 20,
+  },
+  topButton: {
+    width: 40,
+    borderRadius: 4,
+    marginRight: 10, 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  filterBox: {
+    borderColor: 'gray',
+    borderWidth: .4,
+    height: 30,
+    width: '70%',
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  listSection: {
+    maxHeight: 670,
+    width: '100%',
+  },
+  showAddButtonOpen: {
+    backgroundColor: '#6E6E6E',
+  },
+  showAddButtonClosed: {
+    backgroundColor: '#50A3DE',
+  },
+  showImportButtonOpen: {
+    backgroundColor: '#6E6E6E',
+  },
+  showImportButtonClosed: {
+    backgroundColor: '#50A3DE',
+  },
+  langButton: {
+    backgroundColor: '#E37932',
+  },
+  killButton: {
+    backgroundColor: '#4d4d4d'
+  },
+  showTrnslButton: {
+    width: 80,
+    marginRight: 10,
+  },
+  textBox: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    height: 50,
+  },
+  item: {
+  },
+  itemEditMode: {
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    height: editHeight,
+    marginBottom: 5,
+  },
+  itemEditTextbox: {
+    height: editHeight,
+    borderColor: 'gray',
+    borderLeftWidth: 1,
+    borderRightWidth: 1,
+    borderTopWidth: .4,
+    borderBottomWidth: .4,
+    marginRight: 10,
+    height: 30,
+    width: 100,
+    textAlign: 'center',
+  },
+  itemEditTextBoxWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  editItemButtonBar: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    height: editHeight
+  },
+  editItemButton: {
     alignItems: 'center',
     justifyContent: 'center',
+    height: editHeight,
+    width: editHeight,
+    borderRadius: 4,
+    marginRight: 10,
   },
+  editItemSaveButton:{
+    backgroundColor: '#50A3DE',
+  },
+  editItemCloseButton: {
+    backgroundColor: '#D6D6D6',
+  },
+  editItemDeleteButton: {
+    backgroundColor: '#E34B4D',
+  },
+  debugText:{
+    color: 'green'
+  },
+  addButton: {
+    width: '90%',
+    marginBottom: 20,
+  },
+  margins: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  headerText: {
+    alignItems: 'flex-start'
+  },
+  inputGroup: {
+    width: '90%',
+    marginBottom: 20,
+  },
+  importTextBox: {
+    width: '100%',
+    height: 300,
+    textAlignVertical: 'top',
+  },
+  importTextboxWrapper: {
+    width: '90%',
+    marginBottom: 5
+  },
+  importExampleText: {
+    fontSize: 10,
+    marginBottom: 5,
+    color: 'gray'
+  },
+  importButtons: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginBottom: 15,
+  },
+  importButton: {
+    width: '40%',
+    marginRight: 10,
+  }
 });
