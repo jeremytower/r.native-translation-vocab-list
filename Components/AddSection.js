7b207ea34e42e8b6b9754539f427fa6ba364bc9f
Jeremy Tower <jeremy_tower@yahoo.com>
2023-05-01 21:21:40 -0400

    add some icons to the new translate buttons to make them a little more intuitive

--- Components/AddSection.js
+++ Components/AddSection.js
@@ -2,6 +2,7 @@ import { Text, TextInput, View, Button, TouchableOpacity } from "react-native";
 import React from "react";
 import { styles } from "../assets/stylesheet";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import IonIcon from "react-native-vector-icons/Ionicons";
 
 const AddSection = (props) => {
   if (props.showAdd) {
@@ -36,6 +37,11 @@ const AddSection = (props) => {
                 size={20}
                 color={props.darkMode ? "#1f1f1f" : "#fff"}
               />
+              <IonIcon
+                name={"arrow-down"}
+                size={20}
+                color={props.darkMode ? "#1f1f1f" : "#fff"}
+              />
             </TouchableOpacity>
           </View>
         </View>
@@ -66,6 +72,11 @@ const AddSection = (props) => {
             >
               <MaterialCommunityIcons
                 name={"google-translate"}
+                size={20}
+                color={props.darkMode ? "#1f1f1f" : "#fff"}
+              />
+              <IonIcon
+                name={"arrow-up"}
                 size={20}
                 color={props.darkMode ? "#1f1f1f" : "#fff"}
               />
